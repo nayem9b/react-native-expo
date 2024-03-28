14bc45c17517318dc505e14c96aa9f421f2cecc6
nayem9b <razwanniam1@gmail.com>
2024-03-29 05:12:40 +0600

    initialized expo app

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your !</Text>
+      <View style={[styles.box, styles.box1]} />
+      <View style={[styles.box, styles.box2]} />
+      <View style={[styles.box, styles.box3]} />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: "yellow",
+  },
+  box1: {
+    backgroundColor: "red",
+  },
+  box2: {
+    backgroundColor: "green",
+  },
+  box3: {
+    backgroundColor: "purple",
   },
 });
