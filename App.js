73fe304866f78f54d8a0a5ef5429cacd72cbab05
nayem9b <razwanniam1@gmail.com>
2024-03-29 05:37:48 +0600

    flex and directions

--- App.js
+++ App.js
@@ -1,37 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your !</Text>
+    <SafeAreaView style={styles.container}>
+
       <View style={[styles.box, styles.box1]} />
       <View style={[styles.box, styles.box2]} />
       <View style={[styles.box, styles.box3]} />
       <StatusBar style="auto" />
-    </View>
-  )
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding :20,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    // flexWrap: "wrap"
   },
   box: {
     height: 150,
     width: 150,
+    marginTop:100,
     backgroundColor: "yellow",
   },
   box1: {
-    backgroundColor: "red",
+    backgroundColor: "yellow",
+    flex: 1,
   },
   box2: {
+    flex: 2,
     backgroundColor: "green",
+    alignSelf : "flex-end"
   },
   box3: {
+    flex: 0.5,
     backgroundColor: "purple",
+    alignSelf: "center"
   },
 });
